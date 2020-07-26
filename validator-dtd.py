177c88ac8b4d7af409fc2f8c3a58ff8453ed71bd
unknown <ahmedbashir.w@gmail.com>
2020-07-26 13:04:12 +0500

    Added one DTD for Header element and validated using lxml

---
+++ validator-dtd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 12:32:05 2020
+
+@author: Ahmed
+"""
+
+
+from lxml import etree
+
+parser = etree.XMLParser(dtd_validation = True)
+tree = etree.parse("resume.xml",parser)
+print(etree.tostring(tree))
